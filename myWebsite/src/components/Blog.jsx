d3ff2a1e44035f1807f8fc1eeebb52d05f9a9eea
cpnewkirk <cpnewkirk@gmail.com>
2024-01-27 22:29:26 -0500

    Add blog functions

--- myWebsite/src/components/Blog.jsx
+++ myWebsite/src/components/Blog.jsx
@@ -1,5 +1,20 @@
+import BlogItem from "./BlogItem";
+import { blogs_arr } from "../data/data";
+
 function Blog() {
-  return <></>;
+  return (
+    <div>
+      {blogs_arr.map((blog) => (
+        <BlogItem
+          key={blog.id}
+          id={blog.id}
+          title={blog.title}
+          body={blog.body}
+          date={blog.date}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default Blog;
--- myWebsite/src/components/BlogItem.jsx
+++ myWebsite/src/components/BlogItem.jsx
@@ -1,5 +1,35 @@
-function BlogItem() {
-  return <></>;
+import { useLocation, useParams, useNavigate } from "react-router-dom";
+import { blogs_arr } from "../data/data";
+
+function BlogItem(blog) {
+  const loc = useLocation();
+  const nav = useNavigate();
+  const { id } = useParams();
+
+  let idName = "";
+  const locationCheck = loc.pathname.includes("/blog/");
+
+  if (locationCheck) {
+    blog = blogs_arr.find((blog) => blog.id == id);
+  }
+
+  function moreDetails(event) {
+    const blogId = blog.id;
+    event.preventDefault();
+    nav(`/blog/${blogId}`);
+  }
+
+  return (
+    <div id={blog.id}>
+      <article>
+        <div>
+          <button onClick={moreDetails}>{blog.title}</button>
+          <div dangerouslySetInnerHTML={{ __html: blog.body }}></div>
+          <p>{blog.date}</p>
+        </div>
+      </article>
+    </div>
+  );
 }
 
 export default BlogItem;
