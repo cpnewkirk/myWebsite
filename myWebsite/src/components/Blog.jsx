7cd80d3adf4a669a543c6d7b691fde20e323e3d0
cpnewkirk <cpnewkirk@gmail.com>
2024-01-27 23:17:50 -0500

    Add contact me functionality

--- myWebsite/src/components/Blog.jsx
+++ myWebsite/src/components/Blog.jsx
@@ -2,14 +2,33 @@ import BlogItem from "./BlogItem";
 import { blogs_arr } from "../data/data";
 
 function Blog() {
+  function onSubmit() {
+    taggedBlogs = blogs_arr.find((blogs) => blog.tags.includes());
+  }
+
   return (
     <div>
+      <form>
+        <label htmlFor="tags">Tag:</label>
+        <select id="tags" name="tags">
+          <option value="Abroad">Abroad {}</option>
+          <option value="International Events">International Events</option>
+          <option value="International Organization">
+            International Organization
+          </option>
+          <option value="International Topic">International Topic</option>
+          <option value="Photography">Photography</option>
+          <option value="Uncategorized">Uncategorized</option>
+        </select>
+        <input type="submit" value="Submit" />
+      </form>
       {blogs_arr.map((blog) => (
         <BlogItem
           key={blog.id}
           id={blog.id}
           title={blog.title}
           body={blog.body}
+          tags={blog.tags}
           date={blog.date}
         />
       ))}
--- myWebsite/src/components/ContactMe.jsx
+++ myWebsite/src/components/ContactMe.jsx
@@ -1,5 +1,45 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faYoutube,
+  faFacebook,
+  faTwitter,
+  faInstagram,
+  faLinkedin,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
+
 function ContactMe() {
-  return <></>;
+  return (
+    <div className="social-container">
+      <h3>Social Follow</h3>
+      <p>Reach out to me at my social media below or email me at</p> <br />
+      <p>cpnewkirk@gmail.com</p>
+      <a
+        href="https://www.facebook.com/christian.newkirk.77/"
+        className="facebook social"
+      >
+        <FontAwesomeIcon icon={faFacebook} size="2x" />
+      </a>
+      <a href="https://twitter.com/cpnewkirk18" className="twitter social">
+        <FontAwesomeIcon icon={faTwitter} size="2x" />
+      </a>
+      <a
+        href="https://www.instagram.com/cpnewkirk"
+        className="instagram social"
+      >
+        <FontAwesomeIcon icon={faInstagram} size="2x" />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/cpnewkirk/"
+        className="linkedin social"
+      >
+        <FontAwesomeIcon icon={faLinkedin} size="2x" />
+      </a>
+      <a href="https://github.com/cpnewkirk" className="github social">
+        <FontAwesomeIcon icon={faGithub} size="2x" />
+      </a>
+    </div>
+  );
 }
 
 export default ContactMe;
